3134e9b920ad1efc3216ead47dcc0b52d059380a
feliasdager <fdage003@fiu.edu>
2022-08-25 00:49:54 -0400

    redid the App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
-function App() {
+
+
+import './App.css';
+import AppointmentPage from "./containers/appointmentsPage/AppointmentPage";
+import { ContactsPage } from "./containers/appointmentsPage/contactsPage/ContactsPage";
+
+function App() { 
+
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  const addAppointment = (title, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        title: title,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
+
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ContactsPage contacts={contacts} addContact={addContact} /> 
+        <AppointmentPage
+              appointments={appointments}
+              addAppointment={addAppointment}
+              contacts={contacts}
+            />
+
     </div>
   );
 }
