773cf8dc09bbe5ba846b95f3194b5b12bacbfc03
feliasdager <fdage003@fiu.edu>
2022-08-28 00:08:26 -0400

    finished the AppointmentPage component

--- src/containers/appointmentsPage/AppointmentPage.js
+++ src/containers/appointmentsPage/AppointmentPage.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
 
-export default function AppointmentPage() {
+export default function AppointmentPage({appointments, contacts, addAppointment}) { 
+
+  const [title, setTitle] = useState(''); 
+  const [contact, setContact] = useState(''); 
+  const [date, setDate] = useState(''); 
+  const [time, setTime] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault(); 
+    addAppointment(title, contact, date, time); 
+    setTitle(''); 
+    setContact(''); 
+    setDate(''); 
+    setTime('');
+  }
+
   return (
-    <div>AppointmentPage</div>
+    <>
+      <section>
+        <h2>Add Appointment</h2>
+        <AppointmentForm 
+          contacts={contacts}
+          title={title}
+          setTitle={setTitle}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={time}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+        />
+      </section> 
+      <hr /> 
+      <section>
+        <h2>Appointments</h2>
+      </section>
+    </>
   )
 }
