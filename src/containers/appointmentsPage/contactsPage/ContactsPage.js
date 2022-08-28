dc3a96ddc4492fe2bd9dfa1ce1b2e0f116c0e48b
feliasdager <fdage003@fiu.edu>
2022-08-27 22:51:21 -0400

    finished the ContactForm component. No bugs yet

--- src/containers/appointmentsPage/contactsPage/ContactsPage.js
+++ src/containers/appointmentsPage/contactsPage/ContactsPage.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react' 
+import { ContactForm } from '../../../components/contactForm/ContactForm';
+import { TileList } from '../../../components/contactForm/tileList/TileList';
 
 
  export const ContactsPage = ({contacts, addContact}) => { 
@@ -43,8 +45,22 @@ import React, {useState, useEffect} from 'react'
     <section>
         <h2>
             Add Contact 
-            {duplicate ? " ' Name already exists": ""}
-        </h2>
+            {duplicate ? " ' Name already exists": ""} 
+            <ContactForm 
+              name = {name} 
+              setName = {setName} 
+              phone = {phone} 
+              setPhone = {setPhone} 
+              email = {email} 
+              setEmail = {setEmail} 
+              handleSubmit = {handleSubmit}
+            />
+        </h2> 
+    </section> 
+    <hr /> 
+    <section>
+      <h2>Contacts</h2>  
+      <TileList tiles={contacts} />
     </section>
     </>
   )
